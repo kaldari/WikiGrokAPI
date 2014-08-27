591a4f4052fa488f23297697b3051ff176d62d81
kaldari <rkaldari@wikimedia.org>
2014-08-27 11:21:43 -0700

    adding record_answer action

--- api.php
+++ api.php
@@ -14,28 +14,46 @@ function getRequest( $key , $default = '' ) {
 	return $default;
 }
 
-$wikigrokdb = new mysqli( $wikigrokdb['host'], $wikigrokdb['user'], $wikigrokdb['pass'], $wikigrokdb['dbname'] );
 $action = getRequest( 'action' , '' );
-$item = intval( getRequest( 'item' , 0 ) );
 $callback = getRequest( 'callback' );
 
 $out = array( 'status' => 'OK' ) ;
 
 if ( $action === 'get_potential_occupations' ) {
 
-        if ( $item ) {
-				$sql = "SELECT occupation FROM potential_occupation WHERE status IS NULL AND item = $item LIMIT 1";
-                $result = $wikigrokdb->query( $sql );
-                if ( !$result ) die( 'There was an error running the query [' . $wikigrokdb->error . '] '.$sql );
-                $x = $result->fetch_array();
-                if ( $x ) {
-                        $out['occupations'] = "$x[0]";
-                } else {
-                        $out['occupations'] = false;
-                }
-        } else {
-                $out['status'] = "Invalid item input: $item";
-        }
+	$candidatesdb = new mysqli( $candidatesdb['host'], $candidatesdb['user'], $candidatesdb['pass'], $candidatesdb['dbname'] );
+
+	$item = intval( getRequest( 'item' , 0 ) );
+	if ( $item ) {
+			$sql = "SELECT occupation FROM potential_occupation WHERE status IS NULL AND item = $item LIMIT 1";
+			$result = $candidatesdb->query( $sql );
+			if ( !$result ) die( 'There was an error running the query [' . $candidatesdb->error . '] '.$sql );
+			$x = $result->fetch_array();
+			if ( $x ) {
+					$out['occupations'] = "$x[0]";
+			} else {
+					$out['occupations'] = false;
+			}
+	} else {
+			$out['status'] = "Invalid item input: $item";
+	}
+
+} else if ( $action === 'record_answer' ) {
+
+	$wikigrokdb = new mysqli( $wikigrokdb['host'], $wikigrokdb['user'], $wikigrokdb['pass'], $wikigrokdb['dbname'] );
+
+	$item_id = $wikigrokdb->real_escape_string( getRequest( 'item_id' ) );
+	$item = $wikigrokdb->real_escape_string( getRequest( 'item' ) );
+	$occupation_id = $wikigrokdb->real_escape_string( getRequest( 'occupation_id' ) );
+	$occupation = $wikigrokdb->real_escape_string( getRequest( 'occupation' ) );
+	$page_name = $wikigrokdb->real_escape_string( getRequest( 'page_name' ) );
+	$correct = intval( getRequest( 'correct', -1 ) );
+
+	if ( $item_id && $occupation_id && ( $correct === 0 || $correct === 1 ) ) {
+		$sql = "INSERT INTO `occupation_log` (`item_id`, `item`, `occupation_id`, `occupation`, `page_name`, `correct`, `timestamp`) VALUES ($item_id, $item, $occupation_id, $occupation, $page_name, $correct, CURRENT_TIMESTAMP)";
+		$result = $wikigrokdb->query( $sql );
+		if ( !$result ) die( 'There was an error running the query [' . $candidatesdb->error . '] '.$sql );
+	}
 
 } else {
 	$out['status'] = "Unknown action $action" ;
